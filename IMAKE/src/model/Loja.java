36999a13279e829b0a1407528f6a11d4bff389d9
Vinícius souza <vinicanal604@gmail.com>
2024-11-23 12:02:54 -0300

    java

--- IMAKE/src/model/Loja.java
+++ IMAKE/src/model/Loja.java
@@ -1,4 +1,74 @@
 package model;
 
 public class Loja {
+    //variaveis
+    private String ID, CNPJ, nome, telefone, email, endereco, senha;
+
+    //contrutor
+    public Loja(String ID, String CNPJ, String nome, String telefone, String email, String endereco, String senha) {
+        this.ID = ID;
+        this.CNPJ = CNPJ;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.endereco = endereco;
+        this.senha = senha;
+    }
+
+    //get e set
+    public String getSenha() {
+        return senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCNPJ() {
+        return CNPJ;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setCNPJ(String CNPJ) {
+        this.CNPJ = CNPJ;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
 }
