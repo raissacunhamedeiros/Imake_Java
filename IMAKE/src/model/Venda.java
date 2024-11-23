ec3ff1d9f26bcaed1fd2341f8a2bcd3e49177b3b
Vinícius souza <vinicanal604@gmail.com>
2024-11-23 20:15:50 -0300

    java

--- IMAKE/src/model/Venda.java
+++ IMAKE/src/model/Venda.java
@@ -1,4 +1,27 @@
 package model;
 
 public class Venda {
+    private Pessoa pessoa;
+    private Loja loja;
+
+    public Venda(Pessoa pessoa, Loja loja) {
+        this.pessoa = pessoa;
+        this.loja = loja;
+    }
+
+    public Loja getLoja() {
+        return loja;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setLoja(Loja loja) {
+        this.loja = loja;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
 }
