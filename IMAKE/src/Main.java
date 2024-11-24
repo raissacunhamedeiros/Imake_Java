47c4596d4f108292ee5869fd20e9e679cf8a83a6
raissacunhamedeiros <raissa_cunha@hotmail.com>
2024-11-24 18:34:27 -0300

    testando a main com um do while - deu certo!

--- IMAKE/src/Main.java
+++ IMAKE/src/Main.java
@@ -5,21 +5,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Produto produtoTeste= new Produto();
         Scanner scanner = new Scanner(System.in);
         ServiceProduto SP = new ServiceProduto();
+        String resp;
 
-        System.out.println("Digite o nome do produto: ");
-        produtoTeste.setNome(scanner.nextLine());
-        System.out.println("Digite o estoque do produto: ");
-        produtoTeste.setEstoque(scanner.nextInt());
-        System.out.println("digite o ID do produto: ");
-        produtoTeste.setIDProduto(scanner.nextLine());
-        System.out.println("Digite o preço do produto: R$ ");
-        produtoTeste.setPreco(scanner.nextFloat());
+        do {
+            Produto produtoTeste= new Produto(); //Precisa ser instanciado dentro do Do while para não sobrescrever o primeiro produto cadastrado
+            System.out.println("Digite o nome do produto: ");
+            produtoTeste.setNome(scanner.next());
+            System.out.println("Digite o estoque do produto: ");
+            produtoTeste.setEstoque(scanner.nextInt());
+            System.out.println("Digite o ID do produto: ");
+            produtoTeste.setIDProduto(scanner.next());
+            System.out.println("Digite o valor do produto: R$ ");
+            produtoTeste.setPreco(scanner.nextFloat());
 
-        SP.cadastrarProduto(produtoTeste);
-        System.out.println(SP.listar());
+            SP.cadastrarProduto(produtoTeste);
+            System.out.println(SP.listar());
 
+            System.out.println("Você gostaria de cadastrar outro produto? [S/N]");
+            resp= scanner.next();
+        } while(resp.equalsIgnoreCase("S"));
     }
 }
