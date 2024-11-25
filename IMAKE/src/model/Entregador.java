8eac556bbbb4b030b45daa40a78737e925b22760
BeatrizColib <beatrizcolib@gmail.com>
2024-11-25 13:49:32 -0300

    metodos entregador

--- IMAKE/src/model/Entregador.java
+++ IMAKE/src/model/Entregador.java
@@ -4,12 +4,16 @@ import Enum.*;
 public class Entregador extends Pessoa{
     private String meioEntrega;
     private final TipoPessoa tipoPessoa = TipoPessoa.ENTREGADOR;
-    //sera sempre tipo entregador, nao podendo ser alterado
+    private int totalEntregas;
+    private double totalComissoes;
 
-    //construtores
-    public Entregador(TipoPessoa tipoPessoa, String nome, String cpf, String telefone, String email, String endereco, String senha){
+
+    //construtor
+    public Entregador(String nome, String cpf, String telefone, String email, String endereco, String senha){
         super(nome, cpf, telefone, email, endereco, senha);
-    } //finalizar const Pessoa
+        this.totalEntregas = 0;
+        this.totalComissoes = 0.0;
+    } //meio de entrega não fica obrigatório p/ cadastro de entregador - pode ser alt. depois
 
     //get e set
     public TipoPessoa getTipoPessoa() {
@@ -20,13 +24,56 @@ public class Entregador extends Pessoa{
         return meioEntrega;
     }
 
+    public int getTotalEntregas() {
+        return totalEntregas;
+    }
+
+    public double getTotalComissoes() {
+        return totalComissoes;
+    }
+
     public void setMeioEntrega(String meioEntrega) {
-        this.meioEntrega = meioEntrega;
+        if (!meioEntrega.equalsIgnoreCase("bicicleta") &&
+        !meioEntrega.equalsIgnoreCase("moto") &&
+        !meioEntrega.equalsIgnoreCase("carro")){
+            System.out.println("Meio inválido! Meios válidos: bicicleta, moto ou carro.");
+        } else {
+            this.meioEntrega = meioEntrega;
+        }
+    }
+
+    //métodos
+    // usei valorVenda - como valor total da venda para gerar a comissão dele
+    public void concluirEntrega(double valorVenda){
+        double comissao;
+        if (valorVenda >= 100.0) {comissao = 10.0;}
+        else {comissao = 7.0;}
+
+        totalEntregas++;
+        totalComissoes += comissao;
+
+        System.out.println("Entrega finalizada. Comissao - R$: " + comissao);
+    }
+
+    public void exibirEntregas(){
+        System.out.println("Total de entregas finalizadas: " + totalEntregas);
+    }
+
+    public void exibirComissoes(){
+        System.out.println("Total de comissões - R$: " + totalComissoes);
     }
 
     //toString
     @Override
     public String toString(){
-        return tipoPessoa + super.toString();
+        return tipoPessoa + super.toString() + " | Meio de Entrega: " + (meioEntrega != null ? meioEntrega : "A definir") +
+                "\nTotal de entregas: " + totalEntregas +
+                "\nTotal comissões - R$: " + String.format("%.2f", totalComissoes);
     }
+
+
+
+
+
+
 }
