65b210698bc9f9159e3d2d3f83b46612a380bf2c
BeatrizColib <beatrizcolib@gmail.com>
2024-11-26 18:28:11 -0300

    serviceCarrinho e carrinho

--- IMAKE/src/model/Carrinho.java
+++ IMAKE/src/model/Carrinho.java
@@ -1,8 +1,45 @@
 package model;
+import service.ServiceCarrinho;
+import model.Produto;
+import java.util.ArrayList;
 
 public class Carrinho {
+    private final ServiceCarrinho serviceCarrinho;
+    //array com os itens de cada carrinho
+    private final ArrayList<ServiceCarrinho.ItemCarrinho> itens = new ArrayList<>();
 
+    public Carrinho(ServiceCarrinho serviceCarrinho) {
+        this.serviceCarrinho = serviceCarrinho;
+    }
 
+    //métodos
+    public void adicionarProduto(Produto produto, int quantidade) {
+        itens.add(serviceCarrinho.new ItemCarrinho(produto, quantidade));
+    }
+
+    public void removerProduto(Produto produto) {
+        for (int i = 0; i < itens.size(); i++) {
+            if (itens.get(i).getProduto().equals(produto)) {
+                itens.remove(i);
+                return; //sai assim que exclue o produto
+            }
+        }
+    }
+
+    public void exibirCarrinho() {
+        System.out.println("Carrinho:");
+        for (ServiceCarrinho.ItemCarrinho item : itens) {
+            System.out.println("->" + item.getProduto().getNome() + " | Preço: R$ " +
+                    item.getProduto().getPreco() + " | Quantidade: " + item.getQuantidade());
+        }
+    }
+    // este metodo é para mostrar o carrinho pra o cliente
+
+    //este é para enviar as info do carrinho, gerando um array com os produtos
+    //p/ ser usado no restante dos codifos
+    public ArrayList<ServiceCarrinho.ItemCarrinho> getItens() {
+        return itens;
+    }
 }
 
 /*
--- IMAKE/src/service/ServiceCarrinho.java
+++ IMAKE/src/service/ServiceCarrinho.java
@@ -1,6 +1,63 @@
 package service;
+import model.Produto;
+import model.Carrinho;
+
+import java.util.ArrayList;
 
 public class ServiceCarrinho {
+    //lista de todos os carrinhos existentes
+    private final ArrayList<Carrinho> carrinhos = new ArrayList<>();
+
+    //encapsulamento
+    public class ItemCarrinho{
+        private Produto produto;
+        private int quantidade;
+
+        public ItemCarrinho(Produto produto, int quantidade){
+            this.produto = produto;
+            this.quantidade = quantidade;
+        }
+
+        public Produto getProduto() {
+            return produto;
+        }
+
+        public int getQuantidade() {
+            return quantidade;
+        }
+
+        public void setQuantidade(int quantidade) {
+            this.quantidade = quantidade;
+        }
+    }
+
+    //Criar carrinho
+    public void adicionarCarrinho(Carrinho carrinho){
+        carrinhos.add(carrinho);
+    }
+
+    //mexer num carrinho criado
+    public void adicionarProduto(Carrinho carrinho, Produto produto, int quantidade){
+        carrinho.adicionarProduto(produto, quantidade); //colocar essa funcao na classe Carrinho
+    }
+
+    public void removerProduto(Carrinho carrinho, Produto produto){
+        carrinho.removerProduto(produto); //criar esse método em Carrinho
+    }
+
+    public double calcularTotal(Carrinho carrinho) {
+        double total = 0.0;
+        for (ItemCarrinho item : carrinho.getItens()){ //terminar em Carrinho
+            total += item.getProduto().getPreco() * item.getQuantidade();
+        } return total;
+    }
+
+    public void exibirCarrinhos(){
+        for (int i = 0; i < carrinhos.size(); i++){
+            System.out.println("Carrinho (" + i + "): ");
+            carrinhos.get(i).exibirCarrinho(); //terminar em Carrinho
+        }
+    }
 }
 
 /*
